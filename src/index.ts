6f3b9e0248b77b38dd37186a3980ed22dd81f54e
Samar Sheikh <78469550+samarsheikh001@users.noreply.github.com>
2024-01-23 23:43:12 +0530

    api done

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import express, { Express, Request, Response } from "express";
+import { MANGA } from "@consumet/extensions";
+
+const app: Express = express();
+const mangasee123 = new MANGA.Mangasee123();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({ success: true });
+});
+
+app.get("/manga/search/:query", async (req, res) => {
+  const data = await mangasee123.search(req.params.query);
+  res.json({ success: true, data });
+});
+
+app.get("/manga/info/:query", async (req, res) => {
+  const data = await mangasee123.fetchMangaInfo(req.params.query);
+  res.json({ success: true, data });
+});
+
+app.get("/manga/chapter/:query", async (req, res) => {
+  const data = await mangasee123.fetchChapterPages(req.params.query);
+  res.json({ success: true, data });
+});
+
+app.listen(port, () =>
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+);
