02228d75030aa636546128cfaa990fdc4215d5be
Samar Sheikh <78469550+samarsheikh001@users.noreply.github.com>
2024-07-30 19:59:30 +0530

    use tor

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response, query } from "express";
 import cors from "cors";
 import { MANGA } from "@consumet/extensions";
 import { getAmazonProducts } from "./amazon-scrape.js";
+import axios, { AxiosAdapter } from "axios";
+import torRequest from "tor-request";
 
 const app: Express = express();
 const mangasee123 = new MANGA.Mangasee123();
@@ -9,6 +11,43 @@ const mangasee123 = new MANGA.Mangasee123();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+// torRequest.setTorAddress("localhost", 9050);
+
+// Custom Axios adapter using tor-request
+const torAdapter: AxiosAdapter = (config: any) => {
+  return new Promise((resolve, reject) => {
+    const method = config.method.toLowerCase();
+    const options = {
+      url: config.url,
+      method: method,
+      headers: config.headers,
+      qs: config.params,
+      body: config.data,
+      encoding: null, // This ensures the body is a Buffer
+    };
+
+    torRequest(options, (err: any, response: any, body: any) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({
+          data: body,
+          status: response.statusCode,
+          statusText: response.statusMessage,
+          headers: response.headers,
+          config: config,
+          request: response.request,
+        });
+      }
+    });
+  });
+};
+
+// Create an Axios instance with the Tor adapter
+const axiosTor = axios.create({
+  adapter: torAdapter,
+});
 
 app.get("/", (req, res) => {
   res.json({ success: true });
